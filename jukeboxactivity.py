a2b69880fb2a7a2f24b71d650b4bac8a2c2995bf
Gonzalo Odiard <godiard@gmail.com>
2012-06-04 17:57:25 -0300

    Set a fake audio sink in the TagReader to avoid conlict using the audio device SL #3628
    Signed-off-by: Gonzalo Odiard <gonzalo@laptop.org>

--- jukeboxactivity.py
+++ jukeboxactivity.py
@@ -596,6 +596,7 @@ class TagReader(gobject.GObject):
         self.pbin = gst.element_factory_make('playbin', 'player')
         fakesink = gst.element_factory_make('fakesink', 'fakesink')
         self.pbin.set_property('video-sink', fakesink)
+        self.pbin.set_property('audio-sink', fakesink)
         #we need to receive signals from the playbin's bus
         self.bus = self.pbin.get_bus()
         #make sure we are watching the signals on the bus
