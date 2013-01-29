sugarlabs/jukebox-activity

commit 874cae39902b8bfb02c115772b95535519aeb253

Remove unused old code
Signed-off-by: Manuel Kaufmann <humitos@gmail.com>

activity.py
@@ -21,12 +21,9 @@
 
 import sys
 import logging
-import tempfile
 from gettext import gettext as _
-import os
 
 from sugar3.activity import activity
-from sugar3.graphics.objectchooser import ObjectChooser
 from sugar3 import mime
 from sugar3.datastore import datastore
 
@@ -35,7 +32,6 @@ from sugar3.graphics.toolbarbox import ToolbarButton
 from sugar3.activity.widgets import StopButton
 from sugar3.activity.widgets import ActivityToolbarButton
 from sugar3.graphics.alert import ErrorAlert
-from sugar3.graphics.alert import Alert
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -47,7 +43,6 @@ from gi.repository import Gtk
 from gi.repository import Gst
 from gi.repository import Gio
 
-import urllib
 from viewtoolbar import ViewToolbar
 from controls import Controls
 from player import GstPlayer
@@ -66,11 +61,14 @@ class JukeboxActivity(activity.Activity):
 
     def __init__(self, handle):
         activity.Activity.__init__(self, handle)
-        self._object_id = handle.object_id
-        self.set_title(_('Jukebox Activity'))
+
         self.player = None
-        self.max_participants = 1
+
+        self._alert = None
         self._playlist_jobject = None
+
+        self.set_title(_('Jukebox Activity'))
+        self.max_participants = 1
 
         toolbar_box = ToolbarBox()
         activity_button = ActivityToolbarButton(self)
@@ -78,22 +76,15 @@ class JukeboxActivity(activity.Activity):
         toolbar_box.toolbar.insert(activity_button, 0)
         self.title_entry = activity_toolbar.title
 
-        # FIXME: I don't know what is the mission of this line
-        # activity_toolbar.stop.hide()
-
-        self.volume_monitor = Gio.VolumeMonitor.get()
-        self.volume_monitor.connect('mount-added', self._mount_added_cb)
-        self.volume_monitor.connect('mount-removed', self._mount_removed_cb)
-
-        _view_toolbar = ViewToolbar()
-        _view_toolbar.connect('go-fullscreen',
-                              self.__go_fullscreen_cb)
-        _view_toolbar.connect('toggle-playlist',
-                              self.__toggle_playlist_cb)
+        view_toolbar = ViewToolbar()
+        view_toolbar.connect('go-fullscreen',
+                             self.__go_fullscreen_cb)
+        view_toolbar.connect('toggle-playlist',
+                             self.__toggle_playlist_cb)
         view_toolbar_button = ToolbarButton(
-            page=_view_toolbar,
+            page=view_toolbar,
             icon_name='toolbar-view')
-        _view_toolbar.show()
+        view_toolbar.show()
         toolbar_box.toolbar.insert(view_toolbar_button, -1)
         view_toolbar_button.show()
 
@@ -104,38 +95,19 @@ class JukeboxActivity(activity.Activity):
         self.set_toolbar_box(toolbar_box)
         toolbar_box.show_all()
 
-        self.connect("key_press_event", self._key_press_event_cb)
+        self.connect('key_press_event', self.__key_press_event_cb)
 
         # We want to be notified when the activity gets the focus or
         # loses it. When it is not active, we don't need to keep
         # reproducing the video
         self.connect('notify::active', self.__notify_active_cb)
 
-        # FIXME: this is related with shared activity and it doesn't work
-        # if handle.uri:
-        #     pass
-        # elif self._shared_activity:
-        #     if self.get_shared():
-        #         pass
-        #     else:
-        #         # Wait for a successful join before trying to get the document
-        #         self.connect("joined", self._joined_cb)
-
         self.update_id = -1
         self.changed_id = -1
         self.seek_timeout_id = -1
         self.player = None
-        self.uri = None
-
-        # README: I changed this because I was getting an error when I
-        # tried to modify self.bin with something different than
-        # Gtk.Bin
-
-        # self.bin = Gtk.HBox()
-        # self.bin.show()
 
         self.canvas = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        self._alert = None
 
         self.playlist_widget = PlayList()
         self.playlist_widget.connect('play-index', self.__play_index_cb)
@@ -153,12 +125,16 @@ class JukeboxActivity(activity.Activity):
         # Create the player just once
         logging.debug('Instantiating GstPlayer')
         self.player = GstPlayer(self.videowidget)
-        self.player.connect("eos", self._player_eos_cb)
-        self.player.connect("error", self._player_error_cb)
+        self.player.connect('eos', self.__player_eos_cb)
+        self.player.connect('error', self.__player_error_cb)
         self.player.connect('play', self.__player_play_cb)
 
         self.p_position = Gst.CLOCK_TIME_NONE
         self.p_duration = Gst.CLOCK_TIME_NONE
+
+        volume_monitor = Gio.VolumeMonitor.get()
+        volume_monitor.connect('mount-added', self.__mount_added_cb)
+        volume_monitor.connect('mount-removed', self.__mount_removed_cb)
 
     def __notify_active_cb(self, widget, event):
         """Sugar notify us that the activity is becoming active or inactive.
@@ -205,7 +181,7 @@ class JukeboxActivity(activity.Activity):
         playlist_width = int(canvas_size.width * PLAYLIST_WIDTH_PROP)
         self.playlist_widget.set_size_request(playlist_width, 0)
 
-    def _key_press_event_cb(self, widget, event):
+    def __key_press_event_cb(self, widget, event):
         keyname = Gdk.keyval_name(event.keyval)
         logging.info("Keyname Press: %s, time: %s", keyname, event.time)
         if self.title_entry.has_focus():
@@ -261,7 +237,7 @@ class JukeboxActivity(activity.Activity):
         self.player.set_uri(path)
         self.player.play()
 
-    def _player_eos_cb(self, widget):
+    def __player_eos_cb(self, widget):
         self.songchange('next')
 
     def _show_error_alert(self, title, msg=None):
@@ -273,46 +249,17 @@ class JukeboxActivity(activity.Activity):
         self._alert.connect('response', self._alert_cancel_cb)
         self._alert.show()
 
-    def _mount_added_cb(self, volume_monitor, device):
+    def __mount_added_cb(self, volume_monitor, device):
+        logging.debug('Mountpoint added. Checking...')
         self.view_area.set_current_page(0)
         self.remove_alert(self._alert)
         self.playlist_widget.update()
 
-    def _mount_removed_cb(self, volume_monitor, device):
+    def __mount_removed_cb(self, volume_monitor, device):
+        logging.debug('Mountpoint removed. Checking...')
         self.view_area.set_current_page(0)
         self.remove_alert(self._alert)
         self.playlist_widget.update()
-
-    def _show_missing_tracks_alert(self, nro):
-        self._alert = Alert()
-        title = _('%s tracks not found.') % nro
-        self._alert.props.title = title
-        self._alert.add_button(Gtk.ResponseType.APPLY, _('Details'))
-        self.add_alert(self._alert)
-        self._alert.connect('response',
-                self.__missing_tracks_alert_response_cb)
-
-    def __missing_tracks_alert_response_cb(self, alert, response_id):
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        vbox.props.valign = Gtk.Align.CENTER
-        label = Gtk.Label(label='')
-        label.set_markup(_('<b>Missing tracks</b>'))
-        vbox.pack_start(label, False, False, 15)
-
-        for track in self.playlist_widget.get_missing_tracks():
-            path = track['url'].replace('journal://', '')\
-                .replace('file://', '')
-            label = Gtk.Label(label=path)
-            vbox.add(label)
-
-        _missing_tracks = Gtk.ScrolledWindow()
-        _missing_tracks.add_with_viewport(vbox)
-        _missing_tracks.show_all()
-
-        self.view_area.append_page(_missing_tracks, None)
-
-        self.view_area.set_current_page(2)
-        self.remove_alert(alert)
 
     def _alert_cancel_cb(self, alert, response_id):
         self.remove_alert(alert)
@@ -332,7 +279,7 @@ class JukeboxActivity(activity.Activity):
         # n-video = 0 if we call it immediately
         GObject.timeout_add(1000, callback)
 
-    def _player_error_cb(self, widget, message, detail):
+    def __player_error_cb(self, widget, message, detail):
         self.player.stop()
         self.player.set_uri(None)
         self.control.set_disabled()
@@ -345,14 +292,6 @@ class JukeboxActivity(activity.Activity):
         msg = _('This "%s" file can\'t be played') % mimetype
         self._switch_canvas(False)
         self._show_error_alert(title, msg)
-
-    def _joined_cb(self, activity):
-        logging.debug("someone joined")
-        pass
-
-    def _shared_cb(self, activity):
-        logging.debug("shared start")
-        pass
 
     def can_close(self):
         # We need to put the Gst.State in NULL so gstreamer can
@@ -418,10 +357,6 @@ class JukeboxActivity(activity.Activity):
                     self.update_id = GObject.timeout_add(self.UPDATE_INTERVAL,
                                                          self.update_scale_cb)
                 self.control.set_button_pause()
-
-    def volume_changed_cb(self, widget, value):
-        if self.player:
-            self.player.player.set_property('volume', value)
 
     def scale_button_press_cb(self, widget, event):
         self.control.button.set_sensitive(False)

commit 611a35366f837f0a5533af2f890af06922b363c8

Keep the Gio.VolumeMonitor instance in memory
We need to keep a reference to Gio.VolumeMonitor from JukeboxActivity
class (we use 'self' for this) to be able to listen to the signals
emitted.

This bug was introduced in 874cae39902b8bfb02c115772b95535519aeb253

Signed-off-by: Manuel Kaufmann <humitos@gmail.com>

activity.py
@@ -131,9 +131,9 @@ class JukeboxActivity(activity.Activity):
 
         self.player.init_view_area(self.videowidget)
 
-        volume_monitor = Gio.VolumeMonitor.get()
-        volume_monitor.connect('mount-added', self.__mount_added_cb)
-        volume_monitor.connect('mount-removed', self.__mount_removed_cb)
+        self._volume_monitor = Gio.VolumeMonitor.get()
+        self._volume_monitor.connect('mount-added', self.__mount_added_cb)
+        self._volume_monitor.connect('mount-removed', self.__mount_removed_cb)
 
         if handle.object_id is None:
             # The activity was launched from scratch. We need to show

playlist.py
@@ -172,7 +172,8 @@ class PlayList(Gtk.ScrolledWindow):
 
     def update(self):
         for tree_item, playlist_item in zip(self.treemodel, self._items):
-            tree_item[2] = self.check_available_media(playlist_item['path'])
+            tree_item[2] = playlist_item['available'] = \
+                self.check_available_media(playlist_item['path'])
 
     def _add_track(self, file_path, title):
         available = self.check_available_media(file_path)